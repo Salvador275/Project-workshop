89f76fe9e15c51cc6cdd4227a5ffd4dc7838b0ba
Salvador275 <sardor-yuldashev-97@mail.ru>
2020-04-21 11:22:51 +0500

    Создать таблицу "Services"

--- lab_2.sql
+++ lab_2.sql
@@ -12,3 +12,21 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.clients
     OWNER to postgres;
+
+-- Table: public.services
+
+-- DROP TABLE public.services;
+
+CREATE TABLE public.services
+(
+    id bigint NOT NULL,
+    name character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT services_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.services
+    OWNER to postgres;
