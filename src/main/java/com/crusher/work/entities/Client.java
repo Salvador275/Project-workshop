ff4090f6480f261538454d8bd27d50a99253f66e
Salvador275 <sardor-yuldashev-97@mail.ru>
2020-05-27 00:48:21 +0500

    finish

--- src/main/java/com/crusher/work/entities/Client.java
+++ src/main/java/com/crusher/work/entities/Client.java
@@ -1,12 +1,18 @@
 package com.crusher.work.entities;
 
-import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "clients")
 public class Client {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
     private Long id;
     private String name;
 
