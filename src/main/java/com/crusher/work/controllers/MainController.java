bb32a0e2cba726970032545ca9715372f3b1cd8d
Salvador275 <sardor-yuldashev-97@mail.ru>
2020-06-03 13:01:37 +0500

    обработка и вывод

--- src/main/java/com/crusher/work/controllers/MainController.java
+++ src/main/java/com/crusher/work/controllers/MainController.java
@@ -1,15 +1,20 @@
 package com.crusher.work.controllers;
 
+import com.crusher.work.model.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "Main page");
+        RestTemplate restTemplate = new RestTemplate();
+        Page page = restTemplate.getForObject("http://www.mocky.io/v2/5c7db5e13100005a00375fda", Page.class);
+        String text = page.getResult().replaceAll("\\s+", "_");
+        model.addAttribute("title", text);
         return "home";
     }
 
